93fc9f9218eaf0256fd3e223e213c1b0a5254e5e
Gaël DIM <dimgael98@gmail.com>
2019-03-24 23:53:19 +0100

    Mise en place de la sauvegarde de ruches

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ window.addEventListener("load", function (event) {
         template: '#ong_config-template', 
         data: function () {
             return {
-                freq_visite : localStorage.getItem("appiculture_freq_visite")
+                freq_visite : localStorage.getItem("appiculture-freq_visite")
             }
         },
         methods: {
             validate: function () {
                 //Sauvegarde des données dans le local storage
-                localStorage.setItem("appiculture_freq_visite", this.freq_visite);
+                localStorage.setItem("appiculture-freq_visite", this.freq_visite);
             },
             
             save_json: function() {
@@ -19,6 +19,43 @@ window.addEventListener("load", function (event) {
 
             import_json: function() {
                 console.log("importing json file ...")
+            }
+        }
+    });
+
+    Vue.component('nouv_rucher', {
+        props: [], 
+        template: '#nouv_rucher-template', 
+        data: function () {
+            return {
+                ruche_id : "",
+                ruche_nb : 0,
+                ruche_desc : "",
+                ruche_freqvisite : 0
+            }
+        },
+        methods: {
+            validate: function () {
+                let tab_ruche = new Array()
+
+                if(localStorage.getItem("appiculture-ruches")){
+                    tab_ruche = JSON.parse(localStorage.getItem("appiculture-ruches"))
+                }
+
+                let ruche = {}
+
+                ruche.id = this.ruche_id
+                ruche.nb = this.ruche_nb
+                ruche.desc = this.ruche_desc
+                ruche.freqvisite = this.ruche_freqvisite
+
+                tab_ruche.push(ruche)
+
+                localStorage.setItem("appiculture-ruches", JSON.stringify(tab_ruche))
+            },
+
+            check: function(){
+                console.log(localStorage.getItem("appiculture-ruches"))
             }
         }
     });
